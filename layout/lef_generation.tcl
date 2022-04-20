90daecc49adc41f8bf824598e4a682acf8ec2a35
yuetingli666 <lyt1314@stanford.edu>
2022-04-20 04:50:29 -0700

    lef_generator scratch

--- layout/lef_generation.tcl
+++ layout/lef_generation.tcl
@@ -1,9 +1,9 @@
+# in magic, alway draw a box and use key "s" to select 
+# the exact inst before running this proc function 
 proc generate_lef {inst_name} 
 {
-	# in magic, alway draw a box and use key "s" to select 
-	# the exact inst before running this proc function 
-	property LEFclass CORE
-	set box_pos [box position]
+    property LEFclass CORE  
+	set box_pos [box position]  
 	set llx [lindex $box_pos  0]    
 	set lly [lindex $box_pos  1]    
     move origin right [$llx]um  
