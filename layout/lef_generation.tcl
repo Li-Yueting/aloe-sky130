4a941737129c8a75383c94792e33a8c713567331
yuetingli666 <lyt1314@stanford.edu>
2022-04-20 04:49:20 -0700

    lef_generator scratch

--- layout/lef_generation.tcl
+++ layout/lef_generation.tcl
@@ -1,15 +1,17 @@
-proc generate_lef {inst_name} {
+proc generate_lef {inst_name} 
+{
 	# in magic, alway draw a box and use key "s" to select 
 	# the exact inst before running this proc function 
 	property LEFclass CORE
 	set box_pos [box position]
-	set llx [lindex $box_pos  0]
-	set lly [lindex $box_pos  1]
-    move origin right [$llx]um
-    move origin bottom -[$lly]um
-    box
-	property LEFsite unithddbl
-	if {$inst_name==pfet}{
+	set llx [lindex $box_pos  0]    
+	set lly [lindex $box_pos  1]    
+    move origin right [$llx]um  
+    move origin bottom -[$lly]um    
+    box 
+    property LEFsite unithddbl  
+    if {$inst_name==pfet}
+    {
 		# GATE
 		findlabel GATE
 		make port
