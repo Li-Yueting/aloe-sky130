781281cd34ee3c63999936b3a56379e8af6042b0
yuetingli666 <lyt1314@stanford.edu>
2022-04-20 04:53:07 -0700

    lef_generator scratch

--- layout/lef_generation.tcl
+++ layout/lef_generation.tcl
@@ -3,34 +3,34 @@
 proc generate_lef {inst_name} 
 {
     property LEFclass CORE  
-	set box_pos [box position]  
-	set llx [lindex $box_pos  0]    
-	set lly [lindex $box_pos  1]    
+    set box_pos [box position]    
+    set llx [lindex $box_pos 0]    
+    set lly [lindex $box_pos 1]    
     move origin right [$llx]um  
     move origin bottom -[$lly]um    
     box 
     property LEFsite unithddbl  
     if {$inst_name==pfet}
     {
-		# GATE
-		findlabel GATE
-		make port
-		# SOURCE
-		findlabel SOURCE
-		make port
-		# DRAIN
-		findlabel DRAIN
-		make port
-		# VPB
-		findlabel VPB
-		make port
-		# VPWR
-		findlabel VPWR
-		make port
-		port class inout
-		# VGND
-		findlabel VGND
-		make port
-		port class inout
+        # GATE
+        findlabel GATE
+        make port
+        # SOURCE
+        findlabel SOURCE
+        make port
+        # DRAIN
+        findlabel DRAIN
+        make port
+        # VPB
+        findlabel VPB
+        make port
+        # VPWR
+        findlabel VPWR
+        make port
+        port class inout
+        # VGND
+        findlabel VGND
+        make port
+        port class inout
 	}	
 }
