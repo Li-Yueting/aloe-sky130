3d48e5af8fb09cc091c2c8d9c4941f98b2c0511d
Xingyu Ni <xingyuni@rice-503-20-north.farmshare.sunet>
2022-05-10 14:47:27 -0700

    4 generation, 70 pop size

--- stemcell_mflowgen/flow/BRG_Top/build/4-cadence-innovus-flowsetup/innovus-foundation-flow/INNOVUS/run_lec.tcl
+++
@@ -1,9 +0,0 @@
-//
-// Innovus Foundation Flow Code Generator, Mon May  9 12:00:31 PDT 2022
-//
-tclmode
-if {[file exists innovus-foundation-flow/vars.tcl]} {
-   source innovus-foundation-flow/vars.tcl
-}
-source innovus-foundation-flow/procs.tcl
-
